7c3c33140a880c8b19b0a6c75f42eaaab5330e70
Leo Yousif <yousifl@slha.org>
2022-04-08 11:47:18 -0500

    added initial XCode Project

---
+++ officeHoursDiscussion/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  officeHoursDiscussion
+//
+//  Created by Leo Yousif on 4/8/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ officeHoursDiscussion/officeHoursDiscussionApp.swift
@@ -0,0 +1,17 @@
+//
+//  officeHoursDiscussionApp.swift
+//  officeHoursDiscussion
+//
+//  Created by Leo Yousif on 4/8/22.
+//
+
+import SwiftUI
+
+@main
+struct officeHoursDiscussionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
